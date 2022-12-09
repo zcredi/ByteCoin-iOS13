f32fdcc315eb5ade3ee7949414e1afa15465789f
zcredi <zcredi@gmail.com>
2022-12-09 16:23:54 +0300

    add Delegate pickerView

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let coinManager = CoinManager()
+    
+    // Определям, сколько столбцов мы хотим в нашем сборщике.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Сколько строк должно быть у этого средства выбора, используе метод:
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
     }
+    
+    // Метод делегирования pickerView(titleForRow:
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    // Метод делегирования. Будет вызываться каждый раз, когда пользователь прокручивает средство выбора
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selecCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selecCurrency)
+    }
 
 }
 
