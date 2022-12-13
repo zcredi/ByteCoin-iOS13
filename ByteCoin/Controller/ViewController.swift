7050ff154a2842ae94b59b5235d5559d7e3be6c6
zcredi <zcredi@gmail.com>
2022-12-13 10:41:01 +0300

    add extension

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,24 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController: UIViewController {
     
     
     var coinManager = CoinManager()
     
-    // Определям, сколько столбцов мы хотим в нашем сборщике.
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // Сколько строк должно быть у этого средства выбора, используе метод:
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return coinManager.currencyArray.count
-    }
-    
-
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
@@ -37,21 +24,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.delegate = self
         
     }
-    
-    // Метод делегирования pickerView(titleForRow:
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return coinManager.currencyArray[row]
-    }
-    
-    // Метод делегирования. Будет вызываться каждый раз, когда пользователь прокручивает средство выбора
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selecCurrency = coinManager.currencyArray[row]
-        coinManager.getCoinPrice(for: selecCurrency)
-    }
 
 }
+
+//MARK: - CoinManagerDelegate
 
 extension ViewController: CoinManagerDelegate {
 
@@ -63,5 +39,41 @@ extension ViewController: CoinManagerDelegate {
     }
     func didFailWithError(error: Error) {
         print(error)
+    }
+}
+
+//MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    
+    // Определям, сколько столбцов мы хотим в нашем сборщике.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Метод делегирования pickerView(titleForRow:
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+}
+
+//MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    
+    // Сколько строк должно быть у этого средства выбора, используе метод:
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    // Метод делегирования. Будет вызываться каждый раз, когда пользователь прокручивает средство выбора
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selecCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selecCurrency)
     }
 }
